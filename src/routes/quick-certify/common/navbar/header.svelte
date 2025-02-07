c9dd58c906f1c3e9745f2aa8400656ae41dc0106
Aashish Dhawan <aashishdhawan@gmail.com>
2025-02-07 15:44:30 +0530

    WIP

---
+++ src/routes/quick-certify/common/navbar/header.svelte
@@ -0,0 +1,39 @@
+<header class="flex flex-col antialiased">
+	<nav class="bg-white border-gray-200 px-4 max-w-7xl w-full mx-auto py-4 darkbg-gray-900 order-1">
+		<div class="flex justify-between items-center">
+			<div class="flex justify-start items-center">
+				<a href="https://flowbite.com" class="flex mr-6">
+					<img
+						src="https://flowbite.s3.amazonaws.com/logo.svg"
+						class="mr-3 h-8"
+						alt="FlowBite Logo" />
+					<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+						>Crownstack</span>
+				</a>
+			</div>
+			<div class="flex justify-between items-center lg:order-2">
+				<ul
+					class="hidden flex-col mt-0 mr-4 w-full text-md font-medium text-gray-900 md:flex md:flex-row dark:text-white">
+					<li>
+						<a
+							href="#"
+							class="py-3 px-4 hover:underline dark:hover:text-primary-500"
+							aria-current="page">Issuer Profile</a>
+					</li>
+					<li>
+						<a
+							href="#"
+							class="py-3 px-4 hover:underline dark:hover:text-primary-500"
+							aria-current="page">Credentials</a>
+					</li>
+					<li>
+						<a
+							href="#"
+							class="py-3 px-4 hover:underline dark:hover:text-primary-500"
+							aria-current="page">Recipients</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</header>
--- src/routes/quick-certify/public/credential/+page.svelte
+++ src/routes/quick-certify/public/credential/+page.svelte
@@ -1,50 +1,9 @@
-<header class="flex flex-col antialiased">
-	<nav class="bg-white border-gray-200 px-4 max-w-7xl w-full mx-auto py-4 darkbg-gray-900 order-1">
-		<div class="flex justify-between items-center">
-			<div class="flex justify-start items-center">
-				<a href="https://flowbite.com" class="flex mr-6">
-					<img
-						src="https://flowbite.s3.amazonaws.com/logo.svg"
-						class="mr-3 h-8"
-						alt="FlowBite Logo" />
-					<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
-						>Crownstack</span>
-				</a>
-			</div>
-			<div class="flex justify-between items-center lg:order-2">
-				<ul
-					class="hidden flex-col mt-0 mr-4 w-full text-md font-medium text-gray-900 md:flex md:flex-row dark:text-white">
-					<li>
-						<a
-							href="#"
-							class="py-3 px-4 hover:underline dark:hover:text-primary-500"
-							aria-current="page">Issuer Profile</a>
-					</li>
-					<li>
-						<a
-							href="#"
-							class="py-3 px-4 hover:underline dark:hover:text-primary-500"
-							aria-current="page">Credentials</a>
-					</li>
-					<li>
-						<a
-							href="#"
-							class="py-3 px-4 hover:underline dark:hover:text-primary-500"
-							aria-current="page">Recipients</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-</header>
-<!--
-  This example requires updating your template:
+<script>
+	import Header from '../../common/navbar/header.svelte';
+	import Footer from '../../common/navbar/footer.svelte';
+</script>
 
-  ```
-  <html class="h-full bg-white">
-  <body class="h-full">
-  ```
--->
+<Header />
 <div class="flex min-h-full flex-col bg-gray-50">
 	<!-- 3 column wrapper -->
 	<div class="mx-auto w-full max-w-7xl grow lg:flex xl:px-2">
@@ -86,9 +45,9 @@
 	<div class="mx-auto w-full max-w-7xl grow lg:flex xl:px-2 bg-gray-50">
 		<!-- Left sidebar & main wrapper -->
 		<div class="flex-1 xl:flex">
-			<div class="py-6 lg:pl-8 xl:w-80 xl:shrink-0 xl:pl-4">
+			<div class="py-6 lg:pl-8 lg:w-96 xl:shrink-0 xl:pl-4">
 				<!-- Left column area -->
-				<div class="overflow-hidden rounded-sm bg-white shadow-sm border border-gray-100">
+				<div class="overflow-hidden rounded-sm bg-white border border-gray-100">
 					<div
 						class="flex flex-col overflow-hidden rounded-sm border border-stroke-dark bg-white p-4 md:p-4 gap-3">
 						<div class="flex justify-between">
@@ -117,119 +76,97 @@
 								Share Your Award
 							</button>
 						</div>
-						<div class="flex flex-col gap-3 md:flex-row">
+						<div class="flex flex-col md:flex-row">
 							<button
-								target="_blank"
 								type="button"
-								class="ant-btn css-1hqd3g1 ant-btn-default ant-btn-color-default ant-btn-variant-outlined ant-btn-lg !flex !items-center !justify-center !min-h-12 !min-w-12 lg:!h-10 lg:!min-h-10 lg:!min-w-10 flex-1"
-								><span class="ant-btn-icon"
-									><span role="img" aria-label="linkedin" class="anticon anticon-linkedin"
-										><svg
-											viewBox="64 64 896 896"
-											focusable="false"
-											data-icon="linkedin"
-											width="1em"
-											height="1em"
-											fill="currentColor"
-											aria-hidden="true"
-											><path
-												d="M847.7 112H176.3c-35.5 0-64.3 28.8-64.3 64.3v671.4c0 35.5 28.8 64.3 64.3 64.3h671.4c35.5 0 64.3-28.8 64.3-64.3V176.3c0-35.5-28.8-64.3-64.3-64.3zm0 736c-447.8-.1-671.7-.2-671.7-.3.1-447.8.2-671.7.3-671.7 447.8.1 671.7.2 671.7.3-.1 447.8-.2 671.7-.3 671.7zM230.6 411.9h118.7v381.8H230.6zm59.4-52.2c37.9 0 68.8-30.8 68.8-68.8a68.8 68.8 0 10-137.6 0c-.1 38 30.7 68.8 68.8 68.8zm252.3 245.1c0-49.8 9.5-98 71.2-98 60.8 0 61.7 56.9 61.7 101.2v185.7h118.6V584.3c0-102.8-22.2-181.9-142.3-181.9-57.7 0-96.4 31.7-112.3 61.7h-1.6v-52.2H423.7v381.8h118.6V604.8z"
-											></path
-											></svg
-										></span
-									></span
-								><span>Add to LinkedIn Profile</span></button>
-							<div class="flex flex-wrap gap-3">
+								class="text-gray-900 w-full bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-sm text-sm px-3 py-2 text-center inline-flex justify-center items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2">
+								Add to Linkedin Profile
+							</button>
+							<div class="flex gap-2">
 								<div class="hidden md:block">
 									<button
 										type="button"
-										class="ant-btn css-1hqd3g1 ant-btn-default ant-btn-color-default ant-btn-variant-outlined ant-btn-lg ant-btn-icon-only !flex !items-center !justify-center !min-h-12 !min-w-12 lg:!h-10 lg:!min-h-10 lg:!min-w-10"
-										><span class="ant-btn-icon"
-											><span role="img" aria-label="download" class="anticon anticon-download"
-												><svg
-													viewBox="64 64 896 896"
-													focusable="false"
-													data-icon="download"
-													width="1em"
-													height="1em"
-													fill="currentColor"
-													aria-hidden="true"
-													><path
-														d="M505.7 661a8 8 0 0012.6 0l112-141.7c4.1-5.2.4-12.9-6.3-12.9h-74.1V168c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v338.3H400c-6.7 0-10.4 7.7-6.3 12.9l112 141.8zM878 626h-60c-4.4 0-8 3.6-8 8v154H214V634c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v198c0 17.7 14.3 32 32 32h684c17.7 0 32-14.3 32-32V634c0-4.4-3.6-8-8-8z"
-													></path
-													></svg
-												></span
-											></span
-										></button>
+										class=" border border-gray-200 hover:border-blue-700 hover:text-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm text-center p-2.5 inline-flex items-center dark:border-blue-500 dark:text-gray-500 dark:hover:text-blue-800 dark:focus:ring-blue-800 dark:hover:bg-blue-500">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke-width="2"
+											stroke="currentColor"
+											class="size-4">
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+										</svg>
+
+										<span class="sr-only">Icon description</span>
+									</button>
 								</div>
 								<div class="flex-1 md:hidden">
 									<button
 										type="button"
-										class="ant-btn css-1hqd3g1 ant-btn-default ant-btn-color-default ant-btn-variant-outlined ant-btn-lg !flex !items-center !justify-center !min-h-12 !min-w-12 lg:!h-10 lg:!min-h-10 lg:!min-w-10 w-full"
-										><span class="ant-btn-icon"
-											><span role="img" aria-label="download" class="anticon anticon-download"
-												><svg
-													viewBox="64 64 896 896"
-													focusable="false"
-													data-icon="download"
-													width="1em"
-													height="1em"
-													fill="currentColor"
-													aria-hidden="true"
-													><path
-														d="M505.7 661a8 8 0 0012.6 0l112-141.7c4.1-5.2.4-12.9-6.3-12.9h-74.1V168c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v338.3H400c-6.7 0-10.4 7.7-6.3 12.9l112 141.8zM878 626h-60c-4.4 0-8 3.6-8 8v154H214V634c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v198c0 17.7 14.3 32 32 32h684c17.7 0 32-14.3 32-32V634c0-4.4-3.6-8-8-8z"
-													></path
-													></svg
-												></span
-											></span
-										><span>Download</span></button>
+										class=" border border-gray-200 hover:border-blue-700 hover:text-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm text-center p-2.5 inline-flex items-center dark:border-blue-500 dark:text-gray-500 dark:hover:text-blue-800 dark:focus:ring-blue-800 dark:hover:bg-blue-500">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke-width="2"
+											stroke="currentColor"
+											class="size-4">
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+										</svg>
+
+										<span class="sr-only">Icon description</span>
+									</button>
 								</div>
 								<span
 									><button
 										type="button"
-										class="ant-btn css-1hqd3g1 ant-btn-default ant-btn-color-default ant-btn-variant-outlined ant-btn-lg ant-btn-icon-only !flex !items-center !justify-center !min-h-12 !min-w-12 lg:!h-10 lg:!min-h-10 lg:!min-w-10"
-										><span class="ant-btn-icon"
-											><span role="img" aria-label="link" class="anticon anticon-link"
-												><svg
-													viewBox="64 64 896 896"
-													focusable="false"
-													data-icon="link"
-													width="1em"
-													height="1em"
-													fill="currentColor"
-													aria-hidden="true"
-													><path
-														d="M574 665.4a8.03 8.03 0 00-11.3 0L446.5 781.6c-53.8 53.8-144.6 59.5-204 0-59.5-59.5-53.8-150.2 0-204l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3l-39.8-39.8a8.03 8.03 0 00-11.3 0L191.4 526.5c-84.6 84.6-84.6 221.5 0 306s221.5 84.6 306 0l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3L574 665.4zm258.6-474c-84.6-84.6-221.5-84.6-306 0L410.3 307.6a8.03 8.03 0 000 11.3l39.7 39.7c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c53.8-53.8 144.6-59.5 204 0 59.5 59.5 53.8 150.2 0 204L665.3 562.6a8.03 8.03 0 000 11.3l39.8 39.8c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c84.5-84.6 84.5-221.5 0-306.1zM610.1 372.3a8.03 8.03 0 00-11.3 0L372.3 598.7a8.03 8.03 0 000 11.3l39.6 39.6c3.1 3.1 8.2 3.1 11.3 0l226.4-226.4c3.1-3.1 3.1-8.2 0-11.3l-39.5-39.6z"
-													></path
-													></svg
-												></span
-											></span
-										></button
-									></span>
+										class=" border border-gray-200 hover:border-blue-700 hover:text-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm text-center p-2.5 inline-flex items-center dark:border-blue-500 dark:text-gray-500 dark:hover:text-blue-800 dark:focus:ring-blue-800 dark:hover:bg-blue-500">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke-width="1.5"
+											stroke="currentColor"
+											class="size-4">
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
+										</svg>
+
+										<span class="sr-only">Icon description</span>
+									</button></span>
 							</div>
 						</div>
-						<div class="flex flex-col gap-2">
+						<div class="flex flex-col gap-2 mt-2">
 							<p class="text-sm leading-6">Want to report a typo or a mistake?</p>
-							<button
-								type="button"
-								class="ant-btn css-733ipo ant-btn-link ant-btn-color-primary ant-btn-variant-link ant-btn-lg !flex !h-max w-max !items-center !justify-center !py-0"
-								><span class="ant-btn-icon"
-									><span role="img" aria-label="mail" class="anticon anticon-mail"
-										><svg
-											viewBox="64 64 896 896"
-											focusable="false"
-											data-icon="mail"
-											width="1em"
-											height="1em"
-											fill="currentColor"
-											aria-hidden="true"
-											><path
-												d="M928 160H96c-17.7 0-32 14.3-32 32v640c0 17.7 14.3 32 32 32h832c17.7 0 32-14.3 32-32V192c0-17.7-14.3-32-32-32zm-40 110.8V792H136V270.8l-27.6-21.5 39.3-50.5 42.8 33.3h643.1l42.8-33.3 39.3 50.5-27.7 21.5zM833.6 232L512 482 190.4 232l-42.8-33.3-39.3 50.5 27.6 21.5 341.6 265.6a55.99 55.99 0 0068.7 0L888 270.8l27.6-21.5-39.3-50.5-42.7 33.2z"
-											></path
-											></svg
-										></span
-									></span
-								><span>Contact Issuer</span></button>
+
+							<p class="text-gray-500 dark:text-gray-400">
+								<a
+									href="#"
+									class="inline-flex items-center text-sm text-blue-600 dark:text-blue-500 hover:underline">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="2"
+										stroke="currentColor"
+										class="size-4 me-1">
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+									</svg>
+
+									Contact Issuer
+								</a>
+							</p>
 						</div>
 					</div>
 				</div>
@@ -261,3 +198,4 @@
 		</div>
 	</div>
 </div>
+<Footer />
