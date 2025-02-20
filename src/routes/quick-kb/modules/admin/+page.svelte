39439387f480272d53aa5e724478406c3d22c299
Aashish Dhawan <aashishdhawan@gmail.com>
2025-02-20 10:57:18 +0530

    Added edit in quick kb

--- src/routes/quick-kb/modules/admin/+page.svelte
+++ src/routes/quick-kb/modules/admin/+page.svelte
@@ -90,6 +90,72 @@
 					contrast, and hierarchy. It explains how these principles can be used to create visually
 					pleasing and effective designs.
 				</p>
+				<div class="sm:flex sm:items-center sm:justify-right mt-2">
+					<button
+						data-tooltip-target="tooltip-edit"
+						data-modal-target="editWorkspaceModal"
+						data-modal-toggle="editWorkspaceModal"
+						type="button"
+						class="text-black bg-gray-300 hover:bg-blue-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+						><svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="size-3"
+							><path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+							></path
+							></svg> <span class="sr-only">Icon description</span></button>
+					<div
+						id="tooltip-edit"
+						role="tooltip"
+						class="absolute z-10 inline-block px-2 py-1 text-xs font-medium text-white transition-opacity duration-300 bg-gray-900 rounded shadow-xs tooltip dark:bg-gray-700 opacity-0 invisible"
+						data-popper-placement="top"
+						style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(615px, -569px, 0px);">
+						Edit Module <div
+							class="tooltip-arrow"
+							data-popper-arrow=""
+							style="position: absolute; left: 0px; transform: translate3d(38px, 0px, 0px);">
+						</div>
+					</div>
+					<button
+						data-tooltip-target="tooltip-archive"
+						data-modal-target="popup-modal"
+						data-modal-toggle="popup-modal"
+						type="button"
+						class="text-black bg-gray-300 hover:bg-red-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+						><svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="size-3"
+							><path
+								stroke="currentColor"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
+							></path
+							></svg> <span class="sr-only">Icon description</span></button>
+					<div
+						id="tooltip-archive"
+						role="tooltip"
+						class="absolute z-10 inline-block px-2 py-1 text-xs font-medium text-white transition-opacity duration-300 bg-gray-900 rounded shadow-sm tooltip dark:bg-gray-700 opacity-0 invisible"
+						data-popper-placement="top"
+						style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(637px, -569px, 0px);">
+						Archive Module <div
+							class="tooltip-arrow"
+							data-popper-arrow=""
+							style="position: absolute; left: 0px; transform: translate3d(60px, 0px, 0px);">
+						</div>
+					</div>
+				</div>
 			</div>
 
 			<div class="pb-5 border-b border-gray-200 dark:border-gray-700">
@@ -231,6 +297,144 @@
 					</button>
 				</div>
 			</form>
+		</div>
+	</div>
+</div>
+
+<!-- Edit modal -->
+<div
+	id="editWorkspaceModal"
+	tabindex="-1"
+	aria-hidden="true"
+	class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+	<div class="relative p-4 w-full max-w-xl h-full md:h-auto">
+		<!-- Modal content -->
+		<div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+			<!-- Modal header -->
+			<div class="flex justify-between items-center mb-4 sm:mb-5 dark:border-gray-600">
+				<h3 class="text-lg font-semibold text-gray-900 dark:text-white">Edit workspace</h3>
+				<button
+					type="button"
+					class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-sm text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+					data-modal-toggle="editWorkspaceModal">
+					<svg
+						aria-hidden="true"
+						class="w-5 h-5"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							fill-rule="evenodd"
+							d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+							clip-rule="evenodd"></path
+						></svg>
+					<span class="sr-only">Close modal</span>
+				</button>
+			</div>
+			<!-- Modal body -->
+			<form action="#">
+				<div class="grid gap-4 mb-4">
+					<div>
+						<label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+							>Title</label>
+						<input
+							type="text"
+							name="title"
+							id="title"
+							class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+							value="Onboarding and Account Setup"
+							required="" />
+					</div>
+
+					<div>
+						<label
+							for="description"
+							class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+							>Description</label>
+						<textarea
+							id="message"
+							rows="4"
+							class="block p-1.5 w-full text-sm text-gray-900 bg-gray-50 rounded-sm border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+							value="This is description for module being edited"></textarea>
+					</div>
+				</div>
+				<div class="flex items-center space-x-4">
+					<button
+						type="submit"
+						class="text-white inline-flex items-center justify-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+						Edit Module
+						<span class="sr-only">Edit Module</span>
+					</button>
+					<button
+						data-modal-toggle="editWorkspaceModal"
+						type="button"
+						class="inline-flex justify-center text-gray-500 items-center bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 rounded border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+						Discard
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+<div
+	id="popup-modal"
+	tabindex="-1"
+	class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+	<div class="relative p-4 w-full max-w-md max-h-full">
+		<div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+			<button
+				type="button"
+				class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+				data-modal-hide="popup-modal">
+				<svg
+					class="w-3 h-3"
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 14 14">
+					<path
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+				</svg>
+				<span class="sr-only">Close modal</span>
+			</button>
+			<div class="p-4 md:p-5 text-center">
+				<svg
+					class="mx-auto mb-4 text-red-600 w-12 h-12 dark:text-gray-200"
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 20 20">
+					<path
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+				</svg>
+				<h3 class="text-lg font-bold text-gray-700 dark:text-gray-400">
+					Are you sure you want to archive this module?
+				</h3>
+				<p class="mb-5 font-xs text-gray-500">
+					All the information regarding this module will be hidden and its content will also be
+					hidden. An archived module can be restore any time.
+				</p>
+				<button
+					data-modal-hide="popup-modal"
+					type="button"
+					class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+					Yes, I'm sure
+				</button>
+				<button
+					data-modal-hide="popup-modal"
+					type="button"
+					class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+					>No, cancel</button>
+			</div>
 		</div>
 	</div>
 </div>
