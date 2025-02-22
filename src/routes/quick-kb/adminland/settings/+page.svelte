3529bba5a09675ee086e5236439e4729d93cf12b
Aashish Dhawan <aashishdhawan@gmail.com>
2025-02-22 18:35:17 +0530

    UI

--- src/routes/quick-kb/adminland/settings/+page.svelte
+++ src/routes/quick-kb/adminland/settings/+page.svelte
@@ -52,7 +52,7 @@
 								class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
 							<label
 								for="blue-radio"
-								class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">blue</label>
+								class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Blue</label>
 						</div>
 						<div class="flex items-center me-4">
 							<input
