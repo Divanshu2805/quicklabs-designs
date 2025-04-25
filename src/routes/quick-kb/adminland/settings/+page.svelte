7094822f9e0b1a5f4de37967ed74ec538717ebaf
Aashish Dhawan <aashishdhawan@gmail.com>
2025-04-25 16:43:47 +0530

    quick kb

--- src/routes/quick-kb/adminland/settings/+page.svelte
+++ src/routes/quick-kb/adminland/settings/+page.svelte
@@ -134,10 +134,23 @@
 							class="bg-gray-50 mt-2 me-2 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 							value="Quick KB"
 							required />
-						<button
-							type="submit"
-							class="text-white mt-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-sm w-full sm:w-auto px-5 py-1.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-							>Save</button>
+						<button type="submit" class="btn-primary mt-2">Save</button>
+					</div>
+				</div>
+				<div>
+					<h1 class="text-md font-semibold dark:text-white mt-8">Article Footer</h1>
+					<p class="text-gray-500 dark:text-gray-400 text-xs">
+						Fill in the details if you want to show footer on article page. Leave the fields empty
+						if you do not want to show any footer.
+					</p>
+					<div class="flex flex-row items-start">
+						<textarea
+							type="text"
+							id="footer"
+							class="bg-gray-50 mt-2 me-2 border border-gray-300 h-32 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+							placeholder="More information.."
+							required />
+						<button class="btn-primary mt-2">Save</button>
 					</div>
 				</div>
 			</div>
