a4df5c4a2956f7dfc2dc2bde83980dcdc607b15b
Aashish Dhawan <aashishdhawan@gmail.com>
2025-04-25 18:56:54 +0530

    quick-release

---
+++ src/routes/quick-release/shared/publicnavbar.svelte
@@ -0,0 +1,49 @@
+<header class="bg-gray-50 border-b border-gray-100">
+	<div class="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
+		<div class="relative flex h-16 justify-between">
+			<div class="relative z-10 flex px-2 lg:px-0">
+				<div class="flex shrink-0 items-center">
+					<img
+						class="h-8 w-auto"
+						src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+						alt="Your Company" />
+					<h1 class="lg:ml-4 text-black font-extrabold font-mono py-2">Quick Recruit</h1>
+				</div>
+			</div>
+		</div>
+		<nav class="hidden lg:flex lg:space-x-2 lg:py-2" aria-label="Global">
+			<!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-900 hover:bg-gray-50 hover:text-gray-900" -->
+			<a
+				href="#"
+				class="inline-flex items-center rounded-md bg-gray-200 px-3 py-2 text-sm font-medium text-gray-900"
+				aria-current="page">Change Logs</a>
+			<a
+				href="#"
+				class="inline-flex items-center rounded-md px-3 py-2 text-sm font-medium text-gray-900 hover:bg-gray-50 hover:text-gray-900"
+				>Feedbacks</a>
+			<a
+				href="#"
+				class="inline-flex items-center rounded-md px-3 py-2 text-sm font-medium text-gray-900 hover:bg-gray-50 hover:text-gray-900"
+				>Roadmaps</a>
+		</nav>
+	</div>
+
+	<!-- Mobile menu, show/hide based on menu state. -->
+	<nav class="lg:hidden" aria-label="Global" id="mobile-menu">
+		<div class="space-y-1 px-2 pt-2 pb-3">
+			<!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-900 hover:bg-gray-50 hover:text-gray-900" -->
+			<a
+				href="#"
+				class="block rounded-md bg-gray-100 px-3 py-2 text-base font-medium text-gray-900"
+				aria-current="page">Changelog</a>
+			<a
+				href="#"
+				class="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-50 hover:text-gray-900"
+				>Feedbacks</a>
+			<a
+				href="#"
+				class="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-50 hover:text-gray-900"
+				>Roadmaps</a>
+		</div>
+	</nav>
+</header>
