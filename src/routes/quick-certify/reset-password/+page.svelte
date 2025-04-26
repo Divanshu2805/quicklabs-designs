deaa7f10bbb985c038c8f717f73659b1a4f09344
Aashish Dhawan <aashishdhawan@gmail.com>
2025-04-26 09:37:59 +0530

    ui changes

--- src/routes/quick-certify/reset-password/+page.svelte
+++ src/routes/quick-certify/reset-password/+page.svelte
@@ -24,7 +24,7 @@
 						name="password"
 						id="password"
 						placeholder="••••••••"
-						class="bg-gray-50 border border-gray-300 text-gray-900 rounded-sm sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						class="form-text-field"
 						required="" />
 				</div>
 				<div>
@@ -37,14 +37,11 @@
 						name="confirm-password"
 						id="confirm-password"
 						placeholder="••••••••"
-						class="bg-gray-50 border border-gray-300 text-gray-900 rounded-sm sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						class="form-text-field"
 						required="" />
 				</div>
 
-				<button
-					type="submit"
-					class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-sm text-sm px-5 py-2 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-					>Change Password</button>
+				<button type="submit" class="w-full btn-primary">Change Password</button>
 				<p class="text-sm font-light text-gray-500 dark:text-gray-400 text-center">
 					<a
 						href="/quick-certify/login"
