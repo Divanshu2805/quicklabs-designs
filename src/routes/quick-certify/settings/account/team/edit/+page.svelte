2caef65915cea5f2dae61a570ca3e655059850e3
Aashish Dhawan <aashishdhawan@gmail.com>
2025-08-22 13:58:59 +0530

    Ui changes

--- src/routes/quick-certify/settings/account/team/edit/+page.svelte
+++ src/routes/quick-certify/settings/account/team/edit/+page.svelte
@@ -76,37 +76,11 @@
 							for="user-role"
 							class="inline-flex items-center mb-2 text-sm font-medium text-gray-900 dark:text-white">
 							Active
-							<button
-								type="button"
-								data-tooltip-target="tooltip-active-role"
-								data-tooltip-style="dark"
-								class="ml-1">
-								<svg
-									aria-hidden="true"
-									class="w-4 h-4 text-gray-400 hover:text-gray-900 dark:hover:text-white dark:text-gray-500"
-									fill="currentColor"
-									viewBox="0 0 20 20"
-									xmlns="http://www.w3.org/2000/svg"
-									><path
-										fill-rule="evenodd"
-										d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-										clip-rule="evenodd"></path
-									></svg>
-								<span class="sr-only">Show information</span>
-							</button>
-							<div
-								id="tooltip-active-role"
-								role="tooltip"
-								class="tooltip text-tooltip"
-								data-popper-placement="top"
-								style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(606px, -345.5px, 0px);">
-								Mark member as active or inactive
-								<div
-									class="tooltip-arrow"
-									data-popper-arrow=""
-									style="position: absolute; left: 0px; transform: translate3d(192px, 0px, 0px);">
-								</div>
-							</div>
+							
+							<InfoButton
+								tooltipText="Mark member as active or inactive."
+								id="tooltip-user-active" />
+							
 						</label>
 						<select name="user[role]" id="bucket" class="form-input-field"
 							><option value="data">Active</option>
